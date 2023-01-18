352f7f5896c8b441e90a9cbb84a5bbc624cea376
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-01-18 15:21:35 +0100

    getings emails

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
-
+console.log('Vue OK', Vue);
 
 const app = Vue.createApp({
     name: 'demo API',
-    data () {
+    data(){
         return {
             emails: []
         }
-    },
+    }, 
+
     methods: {
-        getRandomMail(){
+        getRandomEmail(){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(function (response){
-            console.log(response.data.response)
-        });
-            
+            .then((response) => {
+               const email = response.data.response;
+               this.emails.push(email);
+            })
         }
     },
-    mounted(){
+
+    mounted (){
+        for(let i = 0; i < 10; i++){
+            this.getRandomEmail();
+        }
     }
+});
 
-}); 
-
-app.mount('#root')
+app.mount('#root');
